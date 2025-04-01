388c843384b1ebb2fbce952a6086192b18284ae1
Leon Chan <leon.chan@thisisgusto.com>
2025-04-01 11:55:48 +0800

    feat: set up basic loop for taking user's input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, World CPP!" << std::endl;
+    string userInput;
+
+    while (true)
+    {
+        cout << "Enter a command or 'quit' to exit: " << endl;
+        cout << ">>> ";
+        getline(cin, userInput);
+
+        if (userInput == "quit")
+        {
+            cout << "Bye!\n";
+            break;
+        }
+
+        cout << userInput << endl;
+    }
+
     return 0;
 }
