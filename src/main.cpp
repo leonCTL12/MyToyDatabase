b8c36ed558daa9bf3ac4451a72df273a0f31998e
Leon Chan <leonchantinglok@gmail.com>
2025-04-07 03:42:31 +0000

    refactor: delete persistent stoarge, it has no use

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "input_interpreter/input_interpreter.h"
 #include <boost/di.hpp>
 #include "app.h"
-#include "persistent_storage/persistent_storage.h"
 #include "input_interpreter/input_interpreter.h"
 
 namespace di = boost::di;
@@ -11,8 +10,7 @@ namespace di = boost::di;
 int main()
 {
     auto injection = boost::di::make_injector(
-        di::bind<InputInterpreter>().to<InputInterpreter>().in(di::singleton),
-        di::bind<PersistentStorage>().to<PersistentStorage>().in(di::singleton));
+        di::bind<InputInterpreter>().to<InputInterpreter>().in(di::singleton));
 
     auto app = injection.create<App>();
     app.run();
