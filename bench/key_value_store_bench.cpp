69ff57e253e2d6f1043f3251b7b3d8ebe763ff66
Leon Chan <leonchantinglok@gmail.com>
2025-04-14 06:39:34 +0000

    tests: added singlethreaded benchmarking test

--- bench/key_value_store_bench.cpp
+++ bench/key_value_store_bench.cpp
@@ -1,7 +1,8 @@
 #include <benchmark/benchmark.h>
+#include <string>
 #include "key_value_store.h"
 
-// Hello world benchmark for KeyValueStore::put
+// Benchmark for KeyValueStore::put
 static void BM_Put(benchmark::State &state)
 {
     KeyValueStore kv;
@@ -13,6 +14,63 @@ static void BM_Put(benchmark::State &state)
     }
 }
 
-// This macro registers the BM_Put function as a benchmark with Google Benchmark's runtime system
-// Ensuring that when the benchmark executables runs, BM_Put will be discovered and executed
+// Benchmark for KeyValueStore::get
+static void BM_Get(benchmark::State &state)
+{
+    KeyValueStore kv;
+    std::string key = "key";
+    std::string value = "value";
+    kv.put(key, value); // Setup: insert key-value pair
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(kv.get(key));
+    }
+}
+
+// Benchmark for KeyValueStore::deleteKey
+static void BM_DeleteKey(benchmark::State &state)
+{
+    KeyValueStore kv;
+    std::string key = "key";
+    std::string value = "value";
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        // The reason why we have to put within the loop is to prevent keep missing after the first iteration
+        kv.put(key, value); // Setup: insert key-value pair
+        state.ResumeTiming();
+        kv.deleteKey(key);
+    }
+}
+
+// Benchmark for Put-Get sequence
+static void BM_PutGet(benchmark::State &state)
+{
+    KeyValueStore kv;
+    std::string key = "key";
+    std::string value = "value";
+    for (auto _ : state)
+    {
+        kv.put(key, value);
+        benchmark::DoNotOptimize(kv.get(key));
+    }
+}
+
+// Benchmark for Put-Delete sequence
+static void BM_PutDelete(benchmark::State &state)
+{
+    KeyValueStore kv;
+    std::string key = "key";
+    std::string value = "value";
+    for (auto _ : state)
+    {
+        kv.put(key, value);
+        kv.deleteKey(key);
+    }
+}
+
 BENCHMARK(BM_Put);
+BENCHMARK(BM_Get);
+BENCHMARK(BM_DeleteKey);
+BENCHMARK(BM_PutGet);
+BENCHMARK(BM_PutDelete);
